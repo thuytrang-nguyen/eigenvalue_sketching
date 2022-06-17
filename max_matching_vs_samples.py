569cd2f79cadf5a2e7211cc473231f7743872698
Archan Ray <archan@dido.cs.umass.edu>
2022-06-17 15:08:27 -0400

    sketch exps

--- max_matching_vs_samples.py
+++ max_matching_vs_samples.py
@@ -18,77 +18,76 @@ sizes = [5000]
 # epses = list(np.arange(0.01, 0.11, 0.01))
 # epses = [0.1]
 eps = 0.03
-samples = np.array(list(range(100,2500,10)))
+samples = np.array(list(range(100,1100,10)))
 trials = 10
 
-L2_all = []
+#L2_all = []
 EE_all = []
+EE_1p = []
+EE_2p = []
 #E_all = []
 
+
+diag_A = np.zeros(sizes[-1])
+v = np.sort(ggs(1/eps**2))
+diag_A[0:int(1/eps**2)] = v
+diag_A = np.sort(diag_A)[::-1]          # descending order sort
+
+# plt.plot(diag_A)
+# plt.show()
+
+A = np.diag(diag_A)
+
 for j in tqdm(range(len(samples))):
+	"""
 	L2_all = []
 	EE_all = []
 	E_all = []
+	"""
 	for n in sizes:
-		"""
-		diag_A = eps*n*np.ones(n)
-		diag_A[int(1/eps**2)+1:] = 0		
-		diag_A[0] = n
-		diag_A[1] = -n
-		"""
-		diag_A = np.zeros(n)
-		v = ggs(1/eps**2)
-		diag_A[0:int(1/eps**2)] = v
-		diag_A = np.sort(diag_A)[::-1]		#descending order sort
-
-		# plt.plot(diag_A)
-		# plt.show()
-
-		A1 = np.diag(diag_A)
-
-		A2 = np.diag(np.sort(v)[::-1])
-
-		#A_half = sqrtm(A)
-
-		L2 = []
+		#L2 = []
 		EE = []
 		#E = []
 
 		for i in range(trials):
 			# get the sketch matrix
 			if sketch_type == "countSketch":
-				sketch_m = countSketch(A1, s=(int(1/eps**2)), return_type="sketch")
+				sketch_m = countSketch(A, s=samples[j], return_type="sketch")
 			if sketch_type == "denseSketch":
-				sketch_m = denseSketch(A1, s=(int(1/eps**2)), return_type="sketch")
+				sketch_m = denseSketch(A, s=samples[j], return_type="sketch")
 			
 			# check
 			# print(sketch_m @ sketch_m.T)
 			# print(sketch_m.T @ sketch_m)
 			# print(sketch_m.shape)
 			
-			sketched_matrix = sketch_m @ A1 @ sketch_m.T
-
 			# get corresponding L2 error this round
-			L2_error = np.linalg.norm(sketched_matrix - A2)
+			# L2_error = np.linalg.norm(A_half @ sketch_m.T @ sketch_m @ A_half - A)
 			
+			local_mat = np.matmul(sketch_m, np.matmul(A, sketch_m.T))
 			# print(local_mat)	
 			# compute eigenvalue estimate
-			max_est_error = np.max(np.abs(np.sort(v)[::-1] - np.real(np.linalg.eigvals(sketched_matrix))))
+			# est_eig = np.max(np.real(np.linalg.eigvals(np.matmul(sketch_m, np.matmul(A, sketch_m.T)))))
+			est_eigs = np.real(np.linalg.eigvals(local_mat))
+			vals = np.zeros(len(diag_A))
+			vals[0:len(est_eigs)] = est_eigs
+			vals = np.sort(vals)[::-1]
+			max_est_error = np.max(np.abs(diag_A - vals))
 			
 			# print(est_eig)	
 			# exit(0)
 			# compute error
 			# est_error = np.abs(n - est_eig)
 
-			L2.append(L2_error)
+			# L2.append(L2_error)
 			EE.append(max_est_error)
 			#E.append(est_eig)
 	
 		# compute mean of the L2 error and estimation error
-		L2_all.append(np.mean(L2))
+		# L2_all.append(np.mean(L2))
 		EE_all.append(np.mean(EE))
-		#EE_1p.append(np.percentile(EE, 20, axis=0))
-		#EE_2p.append(np.percentile(EE, 80, axis=0))
+		EE_1p.append(np.percentile(EE, 20, axis=0))
+		EE_2p.append(np.percentile(EE, 80, axis=0))
 		#E_all.append(np.mean(E))
 
 	#plt.plot(L2_all, EE_all, label="{:.2f}".format(eps))
@@ -102,21 +101,20 @@ for j in tqdm(range(len(samples))):
 # plt.show()
 # '''
 # plt.plot(L2_all, EE_all)
-#samples = samples/sizes[-1]
+samples = samples/sizes[-1]
 ratios = np.array(EE_all)
-#EE_1p = np.array(EE_1p)
-#EE_2p = np.array(EE_2p)
+EE_1p = np.array(EE_1p)
+EE_2p = np.array(EE_2p)
 #print(ratios, EE_1p, EE_2p)
-#plt.plot(np.log(samples), np.log(ratios))
-#plt.fill_between(np.log(samples), np.log(ratios-EE_1p+1e-60), np.log(ratios+EE_2p+1e-60), alpha=0.2)
-plt.plot(L2_all, EE_all)
-plt.ylabel("mean eig matching error")
-plt.xlabel("mean L2 error")
+plt.plot(np.log(samples), np.log(ratios))
+plt.fill_between(np.log(samples), np.log(ratios-EE_1p+1e-60), np.log(ratios+EE_2p+1e-60), alpha=0.2)
+plt.ylabel("log mean eig matching error")
+plt.xlabel("log proportion of samples")
 # plt.title("L2 error vs eigval error of estimation")
 # plt.legend(loc="upper right")
 dir_to_save = "figures/sketching_trial_results/"
 if not os.path.isdir(dir_to_save):
 	os.makedirs(dir_to_save)
-filename = os.path.join(dir_to_save, sketch_type+"_L2_v_EE_GP.pdf")
+filename = os.path.join(dir_to_save, sketch_type+"_MaxMatch_v_samples_GP.pdf")
 plt.savefig(filename)
 # '''
